7c93326f4c4c69dfe64838371c29f4ef8c5738b6
Jessica Lin <jessica.lin@udacity.com>
2016-02-24 16:36:58 -0800

    Update MainActivity.java

--- app/src/main/java/demo/example/com/customarrayadapter/MainActivity.java
+++ app/src/main/java/demo/example/com/customarrayadapter/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    //CCreating a custom adapter called flavorAdapter
     private AndroidFlavorAdapter flavorAdapter;
 
     @Override
